aa34278c567c8e6522f9a9f2110bd70b3a1f9ca6
Andrés Sánchez <asanchezg811@gmail.com>
2018-10-18 21:03:43 -0600

    Adding permutation functions

--- Killer Sudoku/Killer Sudoku/Program.cs
+++ Killer Sudoku/Killer Sudoku/Program.cs
@@ -4,6 +4,9 @@ using Killer_Sudoku.KillerSudokuSolver;
 using Killer_Sudoku.KillerSudokuBoard;
 using System.Windows.Forms;
 using System.Threading;
+using System.Collections.Generic;
+using Killer_Sudoku.TetrisFigures;
+using System.Linq;
 
 namespace Killer_Sudoku
 {
@@ -15,25 +18,14 @@ namespace Killer_Sudoku
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            /*Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GUI());
+            Application.Run(new GUI());*/
 
 
 
-           /* Board board = new Board(5, "sum", 1);
+            Board board = new Board(5, "sum", 1);
             board.fitTetrisFigures();
-
-            foreach (var item in board.boardFigures)
-            {
-                foreach (var caca in item.Positions)
-                {
-                    ArrayExt.PrintArray(caca.Position);
-                }
-                Console.WriteLine("Operacion " + item.Result);
-            }*/
-            
-
         }
     }
 }
---
+++ Killer Sudoku/Killer Sudoku/TetrisFigures/Permutations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Killer_Sudoku.TetrisFigures
+{
+    public class Permutations
+    {
+        //Function to validate sum permutations
+        public static List<List<int>> GetFigureSumPermutations(int number, int figureSize)
+        {
+            List<List<int>> list = GetSumPermutations(number);
+            return FilterPermutations(list, figureSize);
+        }
+
+        //Function to get sum permutations
+
+        public static List<List<int>> GetSumPermutations(int number)
+        {
+            List<List<int>> list = new List<List<int>>() { new List<int>() {number}};
+            
+            for (int i = 1; i < number; i++)
+            {
+                foreach (var sublist in GetSumPermutations(number-i))
+                {
+                    sublist.Insert(0,i);
+                    list.Add(sublist);
+                }
+            }
+            return list;
+        }
+
+        //Function to filter permutations, given a size
+        public static List<List<int>> FilterPermutations(List<List<int>> listToFilter, int figureSize)
+        {
+            listToFilter = listToFilter.Where(x => x.Count.Equals(figureSize)).ToList();
+            List<List<int>> newList = new List<List<int>>();
+
+            for (int x = 0; x < listToFilter.Count; x++)
+            {
+                bool isEqual = false;
+                
+                for (int y = 0; y < listToFilter.ElementAt(x).Count-1; y++)
+                {
+                    if (listToFilter.ElementAt(x).ElementAt(y).Equals(listToFilter.ElementAt(x).ElementAt(y+1)))
+                    {
+                        isEqual = true;
+                        break;
+                    }
+                }
+
+                if (!isEqual)
+                {
+                    newList.Add(listToFilter.ElementAt(x));
+                }
+            }
+            return newList;
+        }
+    }
+}
+
